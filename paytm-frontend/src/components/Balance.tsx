57effa73748fc484ece27ae5ae063cb5e6e1974d
Ashwani Singh <ashwanisingh3846@gmail.com>
2025-10-03 07:21:21 +0530

    Update Balance.tsx

--- paytm-frontend/src/components/Balance.tsx
+++ paytm-frontend/src/components/Balance.tsx
@@ -1,4 +1,5 @@
-export default function Balance({balance}: {balance: {balance: number| string}}){
+
+export default function Balance({balance}){
     return(
         <div className="flex flex-col justify-center items-center  px-4 rounded-md">
             <div className="text-xl  font-medium  text-left 
